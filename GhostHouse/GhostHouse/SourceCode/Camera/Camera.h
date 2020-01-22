995922447e2b8eacf910bcc50ccca730c0ac8351
hera <hera@PC-03>
2020-01-22 15:08:55 +0900

    コメント削除

--- GhostHouse/GhostHouse/SourceCode/Camera/Camera.h
+++ GhostHouse/GhostHouse/SourceCode/Camera/Camera.h
@@ -19,9 +19,6 @@
 #pragma comment(lib, "d3dx11.lib")
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx10.lib")	//「D3DX～」の定義使用時に必要.
-//avavava
-//lol
-//extern float CameraRotationToRadian;
 
 class CCamera
 {
